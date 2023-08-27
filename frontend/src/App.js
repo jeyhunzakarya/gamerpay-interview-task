8e6565a70a9aee1a4d7acfbea9eb80f73fa22055
jeyhunzakarya <jeyhun.zakarya@gmail.com>
2023-08-27 18:29:58 +0900

    added necessary components for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Flex, TextInput } from '@mantine/core';
+import { Button } from '@mantine/core';
+import { useState } from 'react';
 
 function App() {
+  const [input, setInput] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex
+        gap={"sm"}
+        direction={"column"}
+        m="20px auto 0 auto"
+        w="50%"
+      >
+        <TextInput
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+          placeholder="Type anything"
+          label="User Input"
+          withAsterisk
+        />
+        <Button w="20%" type='submit'>
+          Submit input
+        </Button>
+      </Flex>
     </div>
   );
 }
